ddc504c374147aa2a73d63cba05a46ad9224730e
Maxime <maxime.chevalier@iut-valence.fr>
2016-03-23 15:29:38 +0100

    ajout de la struture de donnee grille

--- main.c
+++ main.c
@@ -7,8 +7,23 @@
 
 #include <stdio.h>
 
+#include "structureDonnee.h"
+
 int main(int argc, char **argv)
 {
+	if(argc != 2)
+	{
+		fprintf(stderr,"Le programme doit etre execute de la facon suivante : batailleNavale tailleGrille\nPar exemple, vous pouvez taper batailleNavale 10\n");
+	}
+	
+	int i;
+	
+	//let valchiffre c = int_of_char c - int_of_char '0';;
+	
+	//source : http://www.geeksforgeeks.org/dynamically-allocate-2d-array-c/
+	grille G = (char **)malloc(taille * sizeof(char *));
+    for (i=0; i<r; i++)
+         G[i] = (char *)malloc(taille * sizeof(char));
 	
 	return 0;
 }
