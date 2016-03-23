3db861fc5895eb2ffe966469da3dd720a681ef41
Heloise-fernandes <heloise.fernandesdealmeida@gmail.com>
2016-03-23 16:02:27 +0100

    Ajout de donnée dans structure et debut fonction rempir

--- remplir.c
+++ remplir.c
@@ -1,1 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "structureDonnee.h"
+
+#define TAILLE_NOM_FICHIER 256
+#define COLORIER 'N'
+
+FILE* ouvrir_fichier()
+{
+	char nomFichier[TAILLE_NOM_FICHIER];
+
+	printf("Quel est votre nom de fichier?");
+	scanf("%s",nomFichier);
+	return fopen(nomFichier,"r");
+}
+
+int complet()
+{
+	return 0;
+}
+
+void remplir(grille g,int n)
+{
+	FILE* fichier = ouvrir_fichier();
+	int i,j;
+	int estRempli = 0;
+	char findechaine = fgetc(fichier);
+	
+	while(!estRempli)
+	{
+		printf("On rempli la grille");
+		while(findechaine!=EOF)
+		{
+			if(findechaine=='(')
+			{
+				fscanf(fichier,"%d",&i);//Lire i
+			    findechaine = fgetc(fichier);//on lit ;
+			    if(findechaine==';')
+				{ 
+					fscanf(fichier,"%d",&j);//Lire j
+					findechaine = fgetc(fichier);//Lire (
+				}
+				else{printf("Le fichier est mal formé :(i1,j1)...(ik,jk)");}
+			 }
+			 else
+			 {printf("Le fichier est mal formé :(i1,j1)...(ik,jk)");}
+			 if(g[i][j] == COLORIER)
+			 {
+				 printf("Des bateaux se supperposent. Vous devez changer de fichier");
+				 break;
+			 }
+			 else{g[i][j] = COLORIER;}
+		}
+		if(complet()){estRempli = 1;}
+	}
+}
+
+
 
--- structureDonnee.h
+++ structureDonnee.h
@@ -1,2 +1,12 @@
+#define NB_PORTE_AVION 1
+#define NB_CROISEUR 2
+#define NB_CONTRE_TORPILLEURS 3
+#define NB_TORPILLEURS 4
+#define T_PORTE_AVION 6
+#define T_CROISEUR 4
+#define T_CONTRE_TORPILLEURS 3
+#define T_TORPILLEURS 2
 
 typedef char** grille;
+
+
